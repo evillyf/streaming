5333c1616198996d2d43636c755bad4bcc11c303
evillyf <evillyf05@gmail.com>
2022-06-01 20:07:18 -0300

    configs

---
+++ streaming/filme/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+# o primeiro é como vai ser no bd e o segundo é como aparece para o user
+LISTA_CATEGORIAS = (
+    ("ANALISES", "Análises"),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTACAO", "Apresentação"),
+    ("OUTROS", "Outros")
+)
+
+# criar filme
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='thumb_filmes')
+    descricao = models.TextField(max_length=1000)
+    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
+
+# criar episódios
+
+
+
+
---
+++ streaming/filme/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import homepage
+
+
+
+urlpatterns = [
+    path('', homepage),
+
+]
+
